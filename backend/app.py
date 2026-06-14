106d0cc34bc644b55c56b7bb34579fa9e94734d8
shubhangi-raj-singh <shubhangiraj683@gmail.com>
2026-06-14 16:10:22 +0530

    Update CORS with Vercel URL

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://pathpilot-ai-pi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
